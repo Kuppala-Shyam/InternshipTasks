dc77a0eb2c765c67cdd043e8ff6bc6355b99cfd3
Shyam <kuppalashyam@gmail.com>
2024-02-12 14:18:19 +0530

    updatedFile

---
+++ Task_3/Geometry/Shape.java
@@ -0,0 +1,12 @@
+package InternPack;
+
+public class Shape {
+	
+	double getCalculateArea() {
+		return 0.0;
+	}
+	double getCalculatePerimeter() {
+		return 0.0;
+	}
+//	double calculatePerimeter();
+}
